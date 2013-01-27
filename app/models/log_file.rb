8371173769aaf5707d3ef9b83349452fe3a2e903
mathfuru <mathfuru@gmail.com>
2013-01-27 10:07:34 +0900

    Forward pos when many logs are exist. (#17)

--- app/models/log_file.rb
+++ app/models/log_file.rb
@@ -7,7 +7,8 @@ class LogFile < ActiveRecord::Base
 
   def save_tails
     fp = open(self.file_name, 'r')
-    fp.seek(self.pos)
+    fp.seek(self.start_pos)
+
     while line = fp.gets
       Log.create(
         :contents => line,
@@ -18,6 +19,21 @@ class LogFile < ActiveRecord::Base
     end
     self.update_attribute(:pos, fp.pos)
     fp.close
+  end
+
+  def limit_of_skip
+    3000
+  end
+
+  # self.pos or forwarded self.pos
+  def start_pos
+    log_size = File.size(self.file_name)
+
+    if self.pos + self.limit_of_skip < log_size
+      log_size - self.limit_of_skip
+    else
+      self.pos
+    end
   end
 
   def self.save_tails
--- spec/models/log_file_spec.rb
+++ spec/models/log_file_spec.rb
@@ -23,4 +23,19 @@ describe LogFile do
       @log_file.save_tails
     end.should change(Log, :count).by(2)
   end
+
+  specify do
+    LogFile.any_instance.stub(:limit_of_skip).and_return(3)
+
+    proc do
+      open(@path, 'a'){|f| f.puts 'foo'}
+      @log_file.save_tails
+    end.should change(Log, :count).by(1)
+
+
+    proc do
+      open(@path, 'a'){|f| f.puts "bar\nabcdef"}
+      @log_file.save_tails
+    end.should change(Log, :count).by(1)
+  end
 end
